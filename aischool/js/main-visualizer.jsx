e010b778ec903d64cda1ab481092c4b00a3365dc
LordSox <mi.ochoadr@hotmail.com>
2026-04-04 14:04:43 -0600

    coprr

--- aischool/js/main-visualizer.jsx
+++ aischool/js/main-visualizer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../visualizadores/llm_black_box_simulation.jsx';
+import App from '../visualizadores/LLM_Simulation.jsx';
 import '../css/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
